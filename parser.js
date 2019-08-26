f1ddc12196cea0dce8ac80fbb11f0f906971cbe3
estarodubov <estarodubov@team.amocrm.com>
2019-08-26 14:05:04 +0300

    Created module 'parse', refactoring

---
+++ parser.js
@@ -0,0 +1,12 @@
+const Parser = require('rss-parser');
+const parser = new Parser();
+let arr = []
+
+module.exports.parse = function(url) {
+  parser.parseURL(url, function(err, data) {
+    data.items.forEach(function(item) {
+        arr.push(item.title)
+    })
+  })
+  return arr
+}
--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const app = express()
-const Parser = require('rss-parser');
-const parser = new Parser();
-var arr = []
+
 let url = 'https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en'
+let p = require('./parser')
 
 app.get("/", function(request, response){
-  parser.parseURL(url, function(err, feed) {
-    feed.items.forEach(function(item) {
-        arr.push(item.title)
-      })
-  })
-  response.send(arr);
+  response.send(p.parse(url));
 })
 app.listen(3000);
